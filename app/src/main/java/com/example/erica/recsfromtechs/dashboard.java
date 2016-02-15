6e87b4e458a55788bf3eca31140435e93caa7d00
Julia Redston <jredston3@gatech.edu>
2016-02-15 18:36:10 -0500

    dashboard displays username

--- app/src/main/java/com/example/erica/recsfromtechs/dashboard.java
+++ app/src/main/java/com/example/erica/recsfromtechs/dashboard.java
@@ -5,8 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 
 public class dashboard extends AppCompatActivity {
 
@@ -25,6 +27,26 @@ public class dashboard extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        String usernameText = "";
+        if(savedInstanceState == null) {
+            //the user name has been passed to the dashboard as an "extra"
+            //works like a map, "user" is our key
+            Log.v("welcome", "saved instance state null");
+            Bundle extras = getIntent().getExtras();
+            usernameText = "" + extras.get("user");
+        } else {
+            Log.v("welcome", "saved instance state not null");
+            usernameText = (String) savedInstanceState.getSerializable("user");
+        }
+        Log.v("welcome", "username is found as "+usernameText);
+
+
+        //This is changing the text at the top of our dashboard to welcome our specific user
+        TextView welcomeTextView =new TextView(this);
+        welcomeTextView =(TextView)findViewById(R.id.welcome);
+        welcomeTextView.setText("Welcome "+usernameText);
+
     }
 
     public void logout(View view) {
